e89445e794225734e9bf8b8b531c2d12edc2bc23
Joey Arhar <jarhar@chromium.org>
2019-01-03 13:44:57 -0800

    worked on puppeteer bug, but i really cant get raw data for any request :(

--- pptr795/server.js
+++ pptr795/server.js
@@ -1,38 +1,68 @@
 // https://github.com/GoogleChrome/puppeteer/issues/795
-
 const http = require('http');
+const path = require('path');
+
 const puppeteer = require('puppeteer');
 
-const ORIGINAL_DATA = Buffer.from(JSON.stringify({'msg': '测试'}));
+const charsetToBuffer = {
+  'utf8': Buffer.from('测试'),
+  'iso-8859-1': Buffer.from(['H', 0xe9, 'l', 'l', 0xf6, ' ', 'w', 0xf8, 'r', 'l', 0xd0]),
+  'us-ascii': Buffer.from('Hello world')
+};
 
+function handleRequest(req, res) {
+  const {root, base} = path.parse(req.url);
+  if (!charsetToBuffer[base]) {
+    res.writeHead(404, {'content-type': 'text/html'});
+    res.end('404 not found: ' + req.url);
+    return;
+  }
 
-function serve() {
-    http.createServer(function (req, res) {
-        // The correct header should be 'application/json; charset=utf-8'
-        // but I change it to 'text/plain' to  REPRODUCE THE BUG
-        //res.setHeader('Content-Type', 'text/plain');
-        res.setHeader('Content-Type', 'application/json; charset=utf-8');
-        res.writeHead(200);
-        res.end(ORIGINAL_DATA);
-    }).listen(8001);
-
+  if (root === 'withcharset') {
+    res.writeHead(200, {'content-type': 'text/html; charset=' + base});
+  } else {
+    res.writeHead(200, {'content-type': 'text/html'});
+  }
+  res.end(charsetToBuffer[base]);
 }
 
-async function responseSuccess(res) {
-    let dataReceived = await res.buffer();
-    console.log('Received:\n', dataReceived);
-    console.log('Original:\n', ORIGINAL_DATA);
-    console.log('\n')
+async function startServer() {
+  return new Promise((resolve, reject) => {
+    const server = http.createServer(handleRequest);
+    server.on('error', err => {
+      console.err('server listen error: ' + err);
+      reject(err);
+    });
+    server.on('listening', () => resolve(server));
+    server.listen(8000, 'localhost');
+  });
 }
 
-async function doRequest() {
-    const browser = await puppeteer.launch({headless: false});
+async function doRequest(browser, charset, withCharset) {
+  return new Promise(async resolve => {
+    const url = `http://localhost:8000/${withCharset ? 'withcharset' : 'nocharset'}/${charset}`;
     const page = await browser.newPage();
-    page.on('response', responseSuccess);
-    let res = await page.goto('http://localhost:8001');
+    page.once('response', async response => {
+      console.log('url: ' + url);
+      console.log('original: ' + charsetToBuffer[charset].toString('hex'));
+      console.log('received: ' + (await response.buffer()).toString('hex'));
+      console.log();
+      resolve();
+    });
+    await page.goto(url);
+  });
 }
 
-
-serve();
-doRequest();
-
+(async () => {
+  const server = await startServer();
+  const browser = await puppeteer.launch({
+    //headless: false,
+    executablePath: '/usr/local/google/home/jarhar/chromium/src/out/Release/chrome'
+  });
+  for (const charset in charsetToBuffer) {
+    await doRequest(browser, charset, true);
+    await doRequest(browser, charset, false);
+  }
+  browser.close();
+  server.close();
+})();
