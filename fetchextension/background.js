a9e03ef96d511dbc1ddb9dacb88bc360e2287a27
Joey Arhar <jarhar@chromium.org>
2019-01-11 16:22:56 -0800

    more fetchextension

--- fetchextension/background.js
+++ fetchextension/background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(tab => {
   chrome.debugger.attach({tabId:tab.id}, '1.0',
-      () => chrome.windows.create({url: 'fetch.html', type: 'popup', width: 600, height: 1000}));
+      () => chrome.windows.create({url: 'fetch.html?' + tab.id, type: 'popup', width: 600, height: 1000}));
 });
--- fetchextension/fetch.js
+++ fetchextension/fetch.js
@@ -1,17 +1,30 @@
+const tabId = parseInt(window.location.search.substring(1));
+
 async function send() {
   const textInput = document.querySelector('#textInput');
   const textOutput = document.querySelector('#textOutput');
   textOutput.value += `\nfetch('${textInput.value}')...\n`;
-  try {
+  console.log('sending runtime.evalute, tabId: ' + tabId + ', window.location.search: ' + window.location.search);
+  chrome.debugger.sendCommand({tabId: tabId}, 'Runtime.evaluate', {expression: `fetch('${textInput.value}')`}, result => {
+    console.log('Runtime.evaluate fetch result: ' + JSON.stringify(result))});
+  /*try {
     const response = await fetch(textInput.value);
     textOutput.value += 'Response.url: ' + response.url + '\n';
     textOutput.value += 'Response.text(): ' + await response.text() + '\n';
   } catch (err) {
     textOutput.value += 'fetch() threw error:\n' + err;
-  }
+  }*/
 }
 
-window.onload = function() {
+async function onEvent(debuggeeId, message, params) {
+  const textOutput = document.querySelector('#textOutput');
+  textOutput.value += 'onEvent()\n';
+  textOutput.value += '  debuggeeId: ' + JSON.stringify(debuggeeId);
+  textOutput.value += '  message: ' + JSON.stringify(message);
+  textOutput.value += '  params: ' + JSON.stringify(params);
+}
+
+window.addEventListener('load', function() {
   const main = document.querySelector('#main');
 
   const textInput = document.createElement('input');
@@ -24,7 +37,6 @@ window.onload = function() {
   button.setAttribute('type', 'button');
   button.setAttribute('value', 'send!');
   button.addEventListener('click', send);
-  //button.setAttribute('onclick', 'send()');
   main.appendChild(button);
 
   const outputDiv = document.createElement('div');
@@ -36,4 +48,10 @@ window.onload = function() {
   textOutput.setAttribute('cols', '80');
   textOutput.value = 'output text field\n';
   outputDiv.appendChild(textOutput);
-}
+
+  chrome.debugger.onEvent.addListener(onEvent);
+});
+
+window.addEventListener('unload', function() {
+  chrome.debugger.detach({tabId: tabId});
+});
